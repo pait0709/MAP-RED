7be10cfde1e29a5c2fcc28ab08a3ada4db5ad3c0
Pavit Singh <pait@JesusBook-Pro.local>
2024-04-16 22:19:27 +0530

    protos-early

---
+++ master.py
@@ -0,0 +1,21 @@
+#when master takes input
+
+#initialise the relevant number of child programs and assign address space
+
+#shard the data, indexes and shi.
+
+
+#call the mappers
+
+
+#when call of mappers is received
+
+#detect failure
+
+#call reducers 
+
+#detect failure
+
+#if failure dont decrease iter
+
+#rinse and repeat till conversion or iterations
